bbf2795f7b66674ec2b97bfcd1f33d24442ff43c
Dome Geiger <56254591+gd8213@users.noreply.github.com>
2021-11-15 18:41:44 +0100

    Arduino: PWM changed to 2kHz

--- Arduino/FirmwareArduino/FirmwareArduino.ino
+++ Arduino/FirmwareArduino/FirmwareArduino.ino
@@ -152,7 +152,7 @@ float GetForceFromMeasurement(int rawValue) {
 void TogglePwmInterrupt(bool enable) {
   // Enable or Disable PWM Interrupts
   if (enable == true) {
-    attachInterrupt(digitalPinToInterrupt(pwmInterruptPin), ISR_PwmInterrupt, CHANGE);
+    attachInterrupt(digitalPinToInterrupt(pwmInterruptPin), ISR_PwmInterrupt, RISING);
   } else {
      detachInterrupt(digitalPinToInterrupt(pwmInterruptPin));
   }
